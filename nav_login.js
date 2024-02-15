b53f4c80acac5b24fae4e284c00c83fb9d274967
Bhanu <bhanu19210@gmail.com>
2024-02-15 17:15:21 +0530

    navigation dash

---
+++ nav_login.js
@@ -0,0 +1,8 @@
+var edit_btn = document.getElementById("edit-coon");
+var edit_open = document.getElementById("edit-open");
+
+edit_btn.onclick = function() {
+    console.log("hi");
+    edit_open.classList.toggle('edit-dis');
+    
+}
